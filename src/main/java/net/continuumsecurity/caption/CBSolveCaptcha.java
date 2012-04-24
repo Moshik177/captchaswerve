bc65f2dc2d2d5e88bed7efa2390d750f6de887a1
Stephen de Vries <stephen@continuumsecurity.net>
2012-04-24 13:45:07 +0200

    extracted constructor to avoid too many logins

--- src/main/java/net/continuumsecurity/caption/CBSolveCaptcha.java
+++ src/main/java/net/continuumsecurity/caption/CBSolveCaptcha.java
@@ -9,6 +9,11 @@ import net.continuumsecurity.captcha.caption.CaptchaSolver;
 
 public class CBSolveCaptcha implements ISolveCaptcha {
 	public final static String CHARSET="UTF-8";
+	private CaptchaSolver solver;
+	
+	public void CBSolveCaptcha() {
+		 solver = new CaptchaSolver();
+	}
 	
 	public String solveFromUrl(String strUrl) {
 		String result = null;
@@ -17,9 +22,9 @@ public class CBSolveCaptcha implements ISolveCaptcha {
 			URLConnection conn = url.openConnection();
 			conn.setRequestProperty("Accept-Charset",  CHARSET);
 			InputStream response = conn.getInputStream();
-			CaptchaSolver cs = new CaptchaSolver();
-			cs.initialize();
-			return(cs.solveCaptchaFromUrl(response));
+			
+			solver.initialize();
+			return(solver.solveCaptchaFromUrl(response));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
