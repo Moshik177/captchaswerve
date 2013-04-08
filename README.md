08abb1dbdf2d18d77c568751a90c3cc1f08019d9
stephendv <stephen@continuumsecurity.net>
2013-04-08 18:25:42 +0200

    readme

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 A simple wrapper around CAPTCHA solving services.  Only supports captchbuster at the moment.
 
 # Install
-Signup at captchabuster, the create a properties file:
+Signup at captchabuster, then create a properties file:
 captchabuster.properties with properties for "login" and "api", e.g.:
 
 login=myusername
-
 api=theKeyTheyAssignToMe
+
+Install it into the local maven repository with:
+
+	mvn install -DskipTests
--- src/test/java/net/continuumsecurity/caption/CBSolveCaptchaTest.java
+++ src/test/java/net/continuumsecurity/caption/CBSolveCaptchaTest.java
@@ -1,9 +1,9 @@
 package net.continuumsecurity.caption;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CBSolveCaptchaTest {
 
@@ -14,7 +14,7 @@ public class CBSolveCaptchaTest {
 	@Test
 	public void test() {
 		ISolveCaptcha solver = new CBSolveCaptcha();
-		assertEquals("7T8uGr",solver.solveFromUrl("http://www.google.com/recaptcha/api/image?c=03AHJ_Vuv75stwxFKNM3yXnLcjmg9goF2lHVDPS8VNY5VMNARi4GLDfjjkk-LO7oiVNT7s2GoOSr0hxaxCt6vo_6BR-au23qhhYPFXOjf0n0NLi_l7BnQUQWnYRq4SUdEyIyawegblX3yzxiuMxAxxLPwsb4qqaa3m-w"));
+		assertEquals("oseux3",solver.solveFromUrl("http://www.twisteddelight.org/captcha.jpg"));
 	}
 
 }
